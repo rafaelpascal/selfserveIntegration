f7d7185ea27d238cb63d62612bacb9721b9f0c0f
rafaelpascal <rafaelpascal234@gmail.com>
2023-03-29 20:19:17 +0100

    made a new change to model

--- selfserveApi/connection/connectDB.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Option 3: Passing parameters separately (other dialects)
-module.exports = new Sequelize('revotax_tms', 'root', '', {
-    host: 'localhost',
-    dialect: "mysql"
-  });
